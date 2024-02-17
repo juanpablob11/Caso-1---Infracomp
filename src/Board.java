ef8065fa8276f1c92465fb3e2cb780241ae61a61
sa.ramirezv <sa.ramirezv@uniandes.edu.co>
2024-02-17 12:55:31 -0500

    Main y initialezCells

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.CyclicBarrier;
-
 public class Board {
 
     /* Initializes the cells with their initial state and a mailbox
@@ -33,10 +36,52 @@ public class Board {
         });
         this.currentGeneration = 0;
     }
-
-    public void initializeCells(String initialStateFile) {
-        // Implementation to read the file and assign the initial states to the cells
+  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("\n" +
+                        "Ingrese el número de generaciones a simular: ");
+        int generations = scanner.nextInt();
+        
+        scanner.nextLine(); // Consume the remaining line
+        
+        System.out.println("Ingrese el archivo con el estado inicial: ");
+        String initialStateFile = scanner.nextLine();
+        
+        Cell[][] cells = initializeCells(initialStateFile);
+        
+        // More logic to come
+        
+        scanner.close();
     }
+
+    private static Cell[][] initializeCells(String initialStateFile) {
+        Cell[][] cells = null;
+            try (BufferedReader br = new BufferedReader(new FileReader(initialStateFile))) {
+       
+                String line = br.readLine();
+                if (line != null) {
+                    int n = Integer.parseInt(line.trim()); 
+                    int row = 0;
+                    while ((line = br.readLine()) != null && row < n) {
+                        String[] values = line.split(",");
+                        for (int j = 0; j < Math.min(values.length, n); j++) {
+                            cells[row][j] = new Cell("true".equals(values[j].trim()), row);
+                        }
+                        row++;
+                    }
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading file: " + e.getMessage());
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid number format in the first line of the file: " + e.getMessage());
+            }
+
+            return cells;
+        }
+        
+    
 
     public void runSimulation(int numberOfGenerations) {
         // Implementation to start the simulation and handle the generations logic
